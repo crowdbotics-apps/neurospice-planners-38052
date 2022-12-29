b2022b2eb35ac8396622f6df5194bce5d04e8a4e
Crowdbotics <team@crowdbotics.com>
2022-12-29 02:57:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "neurospice_planners_38052",
+  url: "https://neurospice_planners_38052.botics.co",
+  api: "https://neurospice_planners_38052.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
